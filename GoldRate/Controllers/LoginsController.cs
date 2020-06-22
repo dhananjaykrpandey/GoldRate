61aaf3ebac30b9b3c942717b4eac9d4351dabf7d
Dhananjay Kr Pandey <dhananjaykrpandey@outlook.com>
2020-06-22 10:23:01 +0400

    Project Setup

---
+++ GoldRate/Controllers/LoginsController.cs
@@ -0,0 +1,225 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using GoldRate.Models;
+using Org.BouncyCastle.Math.EC.Rfc7748;
+
+namespace GoldRate.Controllers
+{
+    public class LoginsController : Controller
+    {
+        private readonly DbMOHEswatini _context;
+
+        public LoginsController(DbMOHEswatini context)
+        {
+            _context = context;
+        }
+
+        // GET: mLogins
+        public IActionResult Index()
+        {
+            mLogin mLogin = null;
+            return View(mLogin);
+            //return View(await _context.MLogins.ToListAsync());
+        }
+        [HttpPost]
+        public IActionResult Index(mLogin mLogin)
+        {
+            try
+            {
+
+
+                if (ModelState.IsValid)
+                {
+                    mLogin mLogins = null;
+                    mLogins = _context.MLogins.Where(x => x.UserID.ToUpper() == mLogin.UserID.ToUpper() && x.Password == mLogin.Password).FirstOrDefault();
+
+                    //Check the user name and password  
+                    //Here can be implemented checking logic from the database  
+                    ClaimsIdentity identity = null;
+                    bool isAuthenticated = false;
+
+                    if (mLogins != null && (mLogins.Name.Trim() != null && mLogins.Name.Trim() != ""))
+                    {
+
+                        //Create the identity for the user  
+                        identity = new ClaimsIdentity(new[] { new Claim(ClaimTypes.Name, mLogins.UserID.Trim()),
+                                                      new Claim(ClaimTypes.Role, mLogins.Type.Trim()) ,
+                                                      new Claim(ClaimTypes.Email, mLogins.EmailID.Trim()),
+                                                      new Claim(ClaimTypes.GivenName, mLogins.Name.Trim()),
+                                                       new Claim(ClaimTypes.MobilePhone, mLogins.Phone.Trim())},
+                                                      CookieAuthenticationDefaults.AuthenticationScheme);
+
+                        HttpContext.Session.SetString("login", "true");
+                        HttpContext.Session.SetString("UserName", mLogins.Name.Trim());
+                        isAuthenticated = true;
+                    }
+
+                    if (isAuthenticated)
+                    {
+                        var principal = new ClaimsPrincipal(identity);
+                        var login = HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, principal);
+                        return RedirectToAction("DashBoard", "Home");
+                    }
+                    else
+                    {
+                        ModelState.AddModelError(string.Empty, "Invalid User ID Or Password");
+
+                    }
+                }
+                return View();
+
+            }
+            catch (Exception ex)
+            {
+
+                ModelState.AddModelError(string.Empty, "Some Problem occure while sending request please report it." + ex.Message.ToString());
+                return View();
+            }
+        }
+        public async Task<IActionResult> Logout()
+        {
+            await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+            //HttpContext.Session.Clear();
+            foreach (var cookieKey in Request.Cookies.Keys)
+            {
+                Response.Cookies.Delete(cookieKey);
+            }
+
+            return Redirect("/");
+        }
+        // GET: mLogins/Details/5
+        public async Task<IActionResult> Details(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var mLogin = await _context.MLogins
+                .FirstOrDefaultAsync(m => m.UserID == id);
+            if (mLogin == null)
+            {
+                return NotFound();
+            }
+
+            return View(mLogin);
+        }
+
+        // GET: mLogins/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: mLogins/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("UserID,Password,Name,Type,Phone,EmailID")] mLogin mLogin)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(mLogin);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(mLogin);
+        }
+
+        // GET: mLogins/Edit/5
+        public async Task<IActionResult> Edit(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var mLogin = await _context.MLogins.FindAsync(id);
+            if (mLogin == null)
+            {
+                return NotFound();
+            }
+            return View(mLogin);
+        }
+
+        // POST: mLogins/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(string id, [Bind("UserID,Password,Name,Type,Phone,EmailID")] mLogin mLogin)
+        {
+            if (id != mLogin.UserID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(mLogin);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!mLoginExists(mLogin.UserID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(mLogin);
+        }
+
+        // GET: mLogins/Delete/5
+        public async Task<IActionResult> Delete(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var mLogin = await _context.MLogins
+                .FirstOrDefaultAsync(m => m.UserID == id);
+            if (mLogin == null)
+            {
+                return NotFound();
+            }
+
+            return View(mLogin);
+        }
+
+        // POST: mLogins/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(string id)
+        {
+            var mLogin = await _context.MLogins.FindAsync(id);
+            _context.MLogins.Remove(mLogin);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool mLoginExists(string id)
+        {
+            return _context.MLogins.Any(e => e.UserID == id);
+        }
+    }
+}
--- GoldRate/Startup.cs
+++ GoldRate/Startup.cs
@@ -1,13 +1,24 @@
 using System;
 using System.Collections.Generic;
+using System.Globalization;
+using System.IO.Compression;
 using System.Linq;
 using System.Threading.Tasks;
+using GoldRate.Controllers;
+using GoldRate.Models;
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.ResponseCompression;
+using Microsoft.AspNetCore.Rewrite;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Rotativa.AspNetCore;
+using WebMarkupMin.AspNetCore2;
 
 namespace GoldRate
 {
@@ -23,12 +34,162 @@ namespace GoldRate
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            //Old Code/services.AddControllersWithViews();
+            services.AddResponseCaching(options =>
+            {
+                options.MaximumBodySize = 1024;
+                options.UseCaseSensitivePaths = true;
+            });
+            services.Configure<BrotliCompressionProviderOptions>(options =>
+            {
+                options.Level = CompressionLevel.Optimal;
+            });
+            services.Configure<GzipCompressionProviderOptions>(options =>
+            {
+
+                options.Level = CompressionLevel.Optimal;
+            });
+
+            services.AddResponseCompression(options =>
+            {
+                options.EnableForHttps = true;
+                options.MimeTypes = ClsUtility.MimeTypes();
+                options.Providers.Add<BrotliCompressionProvider>();
+                options.Providers.Add<GzipCompressionProvider>();
+
+            });
+
             services.AddControllersWithViews();
+
+            //if (Env.IsEnvironment("Development"))
+            //{
+            services.AddRazorPages().AddRazorRuntimeCompilation();
+
+            //}
+            services.AddWebOptimizer();
+
+            services.AddWebMarkupMin(
+              options =>
+              {
+                  options.AllowMinificationInDevelopmentEnvironment = true;
+                  options.AllowCompressionInDevelopmentEnvironment = true;
+                  options.DisablePoweredByHttpHeaders = true;
+              })
+              .AddHtmlMinification(
+                options =>
+                {
+                    options.MinificationSettings.RemoveRedundantAttributes = true;
+                    options.MinificationSettings.RemoveHttpProtocolFromAttributes = true;
+                    options.MinificationSettings.RemoveHttpsProtocolFromAttributes = true;
+                    // More options if you like so
+                })
+           .AddHttpCompression();
+
+            services.AddDbContext<DbGoldRateContext>(options => options.UseSqlServer(Configuration.GetConnectionString("MOHEswatiniDBConnection")));
+            services.AddMvc();
+
+
+            services.Configure<CookiePolicyOptions>(options =>
+            {
+                // This lambda determines whether user consent for non-essential cookies is needed for a given request.  
+                options.CheckConsentNeeded = context => true;
+                options.MinimumSameSitePolicy = SameSiteMode.None;
+            });
+            services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme).AddCookie(options =>
+            {
+                options.Cookie.HttpOnly = true;
+                options.LoginPath = "/Logins/Index";
+                options.AccessDeniedPath = "/Home/Index";
+                options.ReturnUrlParameter = "/Home/Index";
+                options.SlidingExpiration = true;
+                options.Cookie.SameSite = SameSiteMode.Lax;
+            });
+            services.AddSession(options =>
+            {
+                options.IdleTimeout = TimeSpan.FromSeconds(10);
+                options.Cookie.HttpOnly = true;
+                options.Cookie.MaxAge = TimeSpan.FromSeconds(30);
+                options.Cookie.IsEssential = true;
+            });
+
+            services.ConfigureApplicationCookie(options =>
+            {
+                // Cookie settings
+                options.Cookie.HttpOnly = true;
+
+                options.ExpireTimeSpan = TimeSpan.FromMinutes(5);
+
+                //options.LoginPath = new PathString("/Logins/Index"); ;
+                //options.AccessDeniedPath = new PathString("/Logins/Index"); ;
+                options.SlidingExpiration = true;
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseResponseCompression();
+
+            app.UseRewriter(new RewriteOptions().Add(new RedirectWwwRule()));
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+            app.UseWebOptimizer();
+            app.UseHttpsRedirection();
+
+            app.UseStaticFiles(new StaticFileOptions
+            {
+                OnPrepareResponse = ctx =>
+                {
+                    // Cache static files for 30 days
+                    ctx.Context.Response.Headers.Append("Cache-Control", "public,max-age=600");
+                    ctx.Context.Response.Headers.Append("Expires", DateTime.UtcNow.AddDays(30).ToString("R", CultureInfo.InvariantCulture));
+                }
+            });
+
+
+            app.UseResponseCaching();
+            app.Use(async (context, next) =>
+            {
+                context.Response.GetTypedHeaders().CacheControl =
+                    new Microsoft.Net.Http.Headers.CacheControlHeaderValue()
+                    {
+                        Public = true,
+                        MaxAge = TimeSpan.FromSeconds(100000)
+                    };
+                context.Response.Headers[Microsoft.Net.Http.Headers.HeaderNames.Vary] =
+                    new string[] { "Accept-Encoding" };
+
+                await next();
+            });
+            app.UseCookiePolicy();
+            app.UseSession();
+            app.UseSitemapMiddleware("https://www.arnikainfotech.com");
+            app.UseRobotsTxt(env);
+            app.UseRouting();
+            app.UseAuthentication();
+            app.UseAuthorization();
+            //app.UseMvc();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+            });
+            // var webRootPath = env.WebRootPath;
+            // call rotativa conf passing env to get web root path
+            RotativaConfiguration.Setup(env.WebRootPath, "Rotativa");
+            /*
+             * old code 
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
@@ -51,7 +212,7 @@ namespace GoldRate
                 endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
-            });
+            });*/
         }
     }
 }
